abb174d17e6f299e55118826ccde5ec3203bb474
Lou King <lking@pobox.com>
2020-01-15 05:45:57 -0500

    Clean up menu and About releases link

--- runningroutes/nav.py
+++ runningroutes/nav.py
@@ -48,7 +48,7 @@ def nav_menu():
     # table is only accessible when interest is set
     if g.interest:
         navbar.items.append(View('Edit Routes', 'admin.routetable', interest=g.interest))
-        navbar.items.append(View('User Routes', 'frontend.routes', interest=g.interest))
+        navbar.items.append(View('User View', 'frontend.routes', interest=g.interest))
 
     # event administrative stuff
     if current_user.has_role('event-admin') or current_user.has_role('super-admin'):
@@ -65,16 +65,17 @@ def nav_menu():
 
     # superadmin stuff
     if current_user.has_role('super-admin'):
-        userroles = Subgroup('User/Roles')
+        userroles = Subgroup('Users/Roles')
         navbar.items.append(userroles)
         userroles.items.append(View('Users', 'admin.users'))
         # this doesn't work because https://github.com/jwag956/flask-security/blob/743be9c979b558b4ecfb177dc8117c0bf55e38ed/flask_security/views.py#L464
         # requires forgot_password has anonymous_user_required decorator
         # userroles.items.append(View('Reset PW', 'security.forgot_password'))
         userroles.items.append(View('Roles', 'admin.roles'))
-        userroles.items.append(View('Interests', 'admin.interests'))
+        navbar.items.append(View('Interests', 'admin.interests'))
         navbar.items.append(View('Files', 'admin.files'))
 
+        navbar.items.append(View('My Account', 'security.change_password'))
         navbar.items.append(View('Debug', 'admin.debug'))
 
     # finally for non super-admin
