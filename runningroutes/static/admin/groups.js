99a3e1c9a18e2376f85a9e89f2ed0b1b04effcc6
Lou King <lking@pobox.com>
2020-01-15 05:25:33 -0500

    Admin Guide opens in new tab
    * fixes #71
    * also some cleanup in layout.js

--- runningroutes/static/admin/groups.js
+++ runningroutes/static/admin/groups.js
@@ -25,14 +25,66 @@ function register_group_for_editor(groupname, groupselectselector) {
 
 }
 
+// see https://stackoverflow.com/a/18660968/799921
+function link_is_external(link_element) {
+    return (link_element.host !== window.location.host);
+}
+
+// see https://stackoverflow.com/a/5713807/799921
+function deparam(query){
+  var setValue = function(root, path, value){
+    if(path.length > 1){
+      var dir = path.shift();
+      if( typeof root[dir] == 'undefined' ){
+        root[dir] = path[0] == '' ? [] : {};
+      }
+
+      arguments.callee(root[dir], path, value);
+    }else{
+      if( root instanceof Array ){
+        root.push(value);
+      }else{
+        root[path] = value;
+      }
+    }
+  };
+  var nvp = query.split('&');
+  var data = {};
+  for( var i = 0 ; i < nvp.length ; i++ ){
+    var pair = nvp[i].split('=');
+    var name = decodeURIComponent(pair[0]);
+    var value = decodeURIComponent(pair[1]);
+
+    var path = name.match(/(^[^\[]+)(\[.*\]$)?/);
+    var first = path[1];
+    if(path[2]){
+      //case of 'array[level1]' || 'array[level1][level2]'
+      path = path[2].match(/(?=\[(.*)\]$)/)[1].split('][')
+    }else{
+      //case of 'name'
+      path = [];
+    }
+    path.unshift(first);
+
+    setValue(data, path, value);
+  }
+  return data;
+}
+
 function register_group(groupname, groupselectselector, groupargappendselector) {
     var thisgroupappendselector = _.replace( groupargappendselector, '{groupname}', groupname );
     $( thisgroupappendselector ).click( function(e) {
+        // don't process external links #71
+        if (link_is_external(this)) return;
         e.preventDefault();
         var group = $( groupselectselector ).val();
-        var args = {};
+
+        var basequery =  $(this).attr('href').split('?');
+        var args = {}
+        if (basequery.length > 1) {
+            args = deparam(basequery[1]);
+        }
         args[groupname] = group;
-        // TODO: should really check if there are already args here first
         location.href = $(this).attr('href') + '?' + $.param(args);
     });
 }
--- runningroutes/static/admin/layout.js
+++ runningroutes/static/admin/layout.js
@@ -1,39 +1,3 @@
 $( function() {
     $( "#navigation ul" ).menu();
 });
-
-// // callback when sign-in button response received
-// function signInCallback(authResult) {
-//   if (authResult['code']) {
-//
-//     // Send the code to the server
-//     $.ajax({
-//       type: 'POST',
-//       url: $SCRIPT_ROOT + '/_token',
-//       // Always include an `X-Requested-With` header in every AJAX request,
-//       // to protect against CSRF attacks.
-//       headers: {
-//         'X-Requested-With': 'XMLHttpRequest'
-//       },
-//       contentType: 'application/octet-stream; charset=utf-8',
-//       success: function(result) {
-//         // Handle or verify the server response.
-//         if (result.authorized) {
-//             // load directed page
-//             // this should be window.location.href = result.redirect;
-//             // but the result.redirect value is giving the wrong URL
-//             // TODO: fix this, maybe in loutilities.tables
-//             window.location.href = $SCRIPT_ROOT + '/admin';
-//         } else {
-//             // reload to show the error message
-//             location.reload();
-//         }
-//       },
-//       processData: false,
-//       data: authResult['code']
-//     });
-//   } else {
-//     // There was an error.
-//     alert( 'error response received from Google' );
-//   }
-// }
