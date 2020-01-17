50c26ab9435afa31d25ed5a9a6d59309a826163e
Lou King <lking@pobox.com>
2020-01-17 05:05:51 -0500

    security views are now responsive
    * login, change_password
    * fixes #77

--- runningroutes/nav.py
+++ runningroutes/nav.py
@@ -44,43 +44,34 @@ class NavRenderer(SimpleRenderer):
 def nav_menu():
     navbar = Navbar('nav_menu')
 
-    navbar.items.append(View('Home', 'admin.home', interest=g.interest))
-    # table is only accessible when interest is set
-    if g.interest:
-        navbar.items.append(View('Edit Routes', 'admin.routetable', interest=g.interest))
-        navbar.items.append(View('User View', 'frontend.routes', interest=g.interest))
+    if current_user.is_authenticated:
+        navbar.items.append(View('Home', 'admin.home', interest=g.interest))
+        # table is only accessible when interest is set
+        if g.interest:
+            navbar.items.append(View('Edit Routes', 'admin.routetable', interest=g.interest))
+            navbar.items.append(View('User View', 'frontend.routes', interest=g.interest))
 
-    # event administrative stuff
-    if current_user.has_role('event-admin') or current_user.has_role('super-admin'):
-        pass
+        # superadmin stuff
+        if current_user.has_role('super-admin'):
+            userroles = Subgroup('Users/Roles')
+            navbar.items.append(userroles)
+            userroles.items.append(View('Users', 'admin.users'))
+            # this doesn't work because https://github.com/jwag956/flask-security/blob/743be9c979b558b4ecfb177dc8117c0bf55e38ed/flask_security/views.py#L464
+            # requires forgot_password has anonymous_user_required decorator
+            # userroles.items.append(View('Reset PW', 'security.forgot_password'))
+            userroles.items.append(View('Roles', 'admin.roles'))
+            navbar.items.append(View('Interests', 'admin.interests'))
+            navbar.items.append(View('Files', 'admin.files'))
 
+            navbar.items.append(View('My Account', 'security.change_password'))
+            navbar.items.append(View('Debug', 'admin.debug'))
 
+        # finally for non super-admin
+        else:
+            navbar.items.append(View('My Account', 'security.change_password'))
+            navbar.items.append(View('About', 'admin.sysinfo'))
 
-    # sponsor stuff
-    if current_user.has_role('sponsor-admin') or current_user.has_role('super-admin'):
-        pass
-
-    if current_user.has_role('event-admin') or current_user.has_role('sponsor-admin') or current_user.has_role('super-admin'):
-        pass
-
-    # superadmin stuff
-    if current_user.has_role('super-admin'):
-        userroles = Subgroup('Users/Roles')
-        navbar.items.append(userroles)
-        userroles.items.append(View('Users', 'admin.users'))
-        # this doesn't work because https://github.com/jwag956/flask-security/blob/743be9c979b558b4ecfb177dc8117c0bf55e38ed/flask_security/views.py#L464
-        # requires forgot_password has anonymous_user_required decorator
-        # userroles.items.append(View('Reset PW', 'security.forgot_password'))
-        userroles.items.append(View('Roles', 'admin.roles'))
-        navbar.items.append(View('Interests', 'admin.interests'))
-        navbar.items.append(View('Files', 'admin.files'))
-
-        navbar.items.append(View('My Account', 'security.change_password'))
-        navbar.items.append(View('Debug', 'admin.debug'))
-
-    # finally for non super-admin
     else:
-        navbar.items.append(View('My Account', 'security.change_password'))
         navbar.items.append(View('About', 'admin.sysinfo'))
 
     return navbar
