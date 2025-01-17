3f75697c7257afe275aa890bfd2b5811e68109d3
Lou King <lking@pobox.com>
2025-01-17 08:37:36 -0500

    fab file for docker deployment (#169)

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,3 @@
-###########################################################################################
-# fabfile  -- deployment using Fabric
-#
-#   Copyright 2019 Lou King
-###########################################################################################
 '''
 fabfile  -- deployment using Fabric
 =================================================================
@@ -12,16 +7,16 @@ expecting fabric.json with following content
         "connect_kwargs": {
             "key_filename": sshkeyfilename (export OpenSSH key from puttygen)
         },
-        "user": "runningroutesmgr"
+        "user": "appuser"
     }
 
 execute as follows
 
-    fab -H <target-host> deploy
+    fab -H <target-host> deploy [prod, sandbox
 
 or 
 
-    fab -H <target1>,<target2> deploy
+    fab -H <target1>,<target2> deploy [prod, sandbox]
 
 if you need to check out a particular branch
 
@@ -32,30 +27,24 @@ if you need to check out a particular branch
 from fabric import task
 from invoke import Exit
 
-APP_NAME = 'runningroutes'
-WSGI_SCRIPT = 'runningroutes.wsgi'
+APP_NAME = 'routes'
+PROJECT_NAME = 'runningroutes'
+
+qualifiers = ['prod', 'sandbox']
 
 @task
-def deploy(c, branchname='master'):
-    print('c.user={} c.host={} branchname={}'.format(c.user, c.host, branchname))
+def deploy(c, qualifier, branchname='master'):
+    if qualifier not in qualifiers:
+        raise Exit(f'deploy qualifier parameter must be one of {qualifiers}')
+        
+    print(f'c.user={c.user} c.host={c.host} branchname={branchname}')
 
-    venv_dir = '/var/www/{server}/venv'.format(server=c.host)
-    project_dir = '/var/www/{server}/{appname}/{appname}'.format(server=c.host, appname=APP_NAME)
+    project_dir = f'~/{APP_NAME}-{qualifier}'
 
-    c.run('cd {} && git pull'.format(project_dir))
-    
-    if not c.run('cd {} && git show-ref --verify --quiet refs/heads/{}'.format(project_dir, branchname), warn=True):
-        raise Exit('branchname {} does not exist'.format(branchname))
+    for the_file in ['docker-compose.yml']:
+        if not c.run(f"cd {project_dir} && curl --fail -O 'https://raw.githubusercontent.com/louking/{PROJECT_NAME}/{branchname}/{the_file}'", warn=True):
+            raise Exit(f'louking/{PROJECT_NAME}/{branchname}/{the_file} does not exist')
 
-    c.run('cd {} && git checkout {}'.format(project_dir, branchname))
-    c.run('cd {} && cp -R ../../libs/js  runningroutes/static'.format(project_dir))
-    # must source bin/activate before each command which must be done under venv
-    # because each is a separate process
-    c.run('cd {} && source {}/bin/activate && pip install -r requirements.txt'.format(project_dir, venv_dir))
-    
-    versions_dir = '{}/migrations/versions'.format(project_dir)
-    if not c.run('test -d {}'.format(versions_dir), warn=True):
-        c.run('mkdir -p {}'.format(versions_dir))
-    
-    c.run('cd {} && source {}/bin/activate && flask db upgrade'.format(project_dir, venv_dir))
-    c.run('cd {} && touch {}'.format(project_dir, WSGI_SCRIPT))
+    # stop and build/start docker services
+    c.run(f'cd {project_dir} && docker compose pull')
+    c.run(f'cd {project_dir} && docker compose up -d')
